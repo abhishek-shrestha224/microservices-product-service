6802078849e91e70055df74fc7acf60ac6663b96
Abhishek Shrestha <meabhi224@gmail.com>
2025-02-14 14:06:20 +0000

    added server port updated compose file to use named volume

--- src/main/java/world/hello/product/ProductService.java
+++ src/main/java/world/hello/product/ProductService.java
@@ -4,10 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ProductMsApplication {
+public class ProductService {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ProductMsApplication.class, args);
-	}
-
+  public static void main(String[] args) {
+    SpringApplication.run(ProductService.class, args);
+  }
 }
