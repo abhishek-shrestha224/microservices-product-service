3b5ad530ba9a357f254aa713445b3c69242b2edf
Abhishek Shrestha <meabhi224@gmail.com>
2025-02-15 06:05:08 +0000

    get by id controller

--- src/main/java/world/hello/product/controller/ProductController.java
+++ src/main/java/world/hello/product/controller/ProductController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -41,8 +42,13 @@ public class ProductController {
   @ResponseStatus(HttpStatus.OK)
   public List<Product> getAllProducts() {
     log.info("GET /api/v1/products");
-    final List<Product> products = productService.fetchAllProducts();
-    log.info("Found products: {}", products);
-    return products;
+    return productService.fetchAllProducts();
+  }
+
+  @GetMapping("/{id}")
+  @ResponseStatus(HttpStatus.OK)
+  public Product getProductById(@PathVariable("id") final String id) {
+    log.info("GET /api/v1/products/{}", id);
+    return productService.fetchProductById(id);
   }
 }
